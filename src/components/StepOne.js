b2544b9dbe125f87631ea6590d5405ef8fc1beb1
Seda Kara <sedaakaraa02@gmail.com>
2022-08-16 21:11:42 +0300

    geliştirme yapıldı

--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -8,83 +8,81 @@ let StepOne = (props) => {
   let { form, setForm } = props;
 
   return (
+    <Formik
+      initialValues={form}
+      validationSchema={Yup.object({
+        name: Yup.string().required("İsim boş bırakılamaz"),
+        email: Yup.string().email().required("Email boş bırakılamaz"),
+      })}
+      onSubmit={(values, { resetForm, setSubmitting }) => {
+        console.log(values);
+        setForm(values);
+        setTimeout(() => {
+          resetForm();
+          setSubmitting(false);
+          navigate("/steptwo");
+        }, 2000);
+      }}
+    >
+      {({
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+        dirty,
+        touched,
+        isSubmitting,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <h3>Kaydol</h3>
+          <label htmlFor="name">İsim</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Seda..."
+            className="input"
+            value={values.name}
+            onChange={handleChange}
+          />
+          {errors.name && touched.name && (
+            <div className="input-feedback">{errors.name}</div>
+          )}
+          <label htmlFor="name" className="topMargin">
+            Soyisim
+          </label>
+          <input
+            id="soyisim"
+            type="text"
+            placeholder="Kara"
+            className="input"
+            value={values.soyisim}
+            onChange={handleChange}
+          />
 
-      <Formik
-        initialValues={form}
-        validationSchema={Yup.object({
-          name: Yup.string().required("İsim boş bırakılamaz"),
-          email: Yup.string().email().required("Email boş bırakılamaz"),
-        })}
-        onSubmit={(values, { resetForm, setSubmitting }) => {
-          console.log(values);
-          setForm(values)
-          setTimeout(() => {
-            resetForm();
-            setSubmitting(false);
-            navigate("/steptwo");
-          }, 2000);
-        }}
-      >
-        {({
-          values,
-          errors,
-          handleChange,
-          handleSubmit,
-          handleReset,
-          dirty,
-          touched,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <h3>Kaydol</h3>
-            <label htmlFor="name">İsim</label>
-            <input
-              id="name"
-              type="text"
-              placeholder="Seda..."
-              className="input"
-              value={values.name}
-              onChange={handleChange}
-            />
-            {errors.name && touched.name && (
-              <div className="input-feedback">{errors.name}</div>
-            )}
-              <label htmlFor="name" className="topMargin">
-              Soyisim
-            </label>
-            <input
-              id="soyisim"
-              type="text"
-              placeholder="Kara"
-              className="input"
-              value={values.soyisim}
-              onChange={handleChange}
-            />
+          <label htmlFor="name" className="topMargin">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="sedaakaraa02@gmail.com"
+            className="input"
+            value={values.email}
+            onChange={handleChange}
+          />
 
-            <label htmlFor="name" className="topMargin">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder="sedaakaraa02@gmail.com"
-              className="input"
-              value={values.email}
-              onChange={handleChange}
-            />
-            
-            {errors.email && touched.email && (
-              <div className="input-feedback">{errors.email}</div>
-            )}
-            
-            <button type="submit" disabled={!dirty || isSubmitting}>
-              Sonra
-            </button>
-          </form>
-        )}
-      </Formik>
-    
+          {errors.email && touched.email && (
+            <div className="input-feedback">{errors.email}</div>
+          )}
+
+          <button type="submit" disabled={!dirty || isSubmitting}>
+            Sonra
+          </button>
+        </form>
+      )}
+    </Formik>
   );
-}
+};
 
 export default StepOne;
+//* butonuma veri girildiğinde ve submit olduysa aslında butonum aktifleştiyse bu komut çalışır. *//
